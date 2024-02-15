3233b959cf9d24cead814acd5fb453d0c47f4f64
Kody <kydokody@gmail.com>
2024-02-15 13:58:20 +0100

    EX014

--- codeBattle.py
+++ codeBattle.py
@@ -170,3 +170,11 @@ user_to_upper = str(input("Texte à passer en majuscules: "))
 
 for letter in user_to_upper:
     print(letter.upper())
+
+# EXERCICE 14:
+
+n = 10
+
+for i in range(n, 0, -1):
+    print(i)
+
---
+++ compileExos.py
@@ -0,0 +1,16 @@
+# Playground to execute py codeBattle exercices
+
+# EXERCICE 13:
+#
+# user_to_upper = str(input("Texte à passer en majuscules: "))
+#
+# for letter in user_to_upper:
+#     print(letter.upper())
+
+# EXERCICE 14:
+
+n = 10
+
+for i in range(n, 0, -1):
+    print(i)
+
